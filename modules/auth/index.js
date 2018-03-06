98f1c3b18d988cabad1414640e7b815bcc6ebfe7
Mathias <mathiassoeholm@gmail.com>
2018-03-06 22:07:19 +0100

    Added empty scripts

---
+++ modules/auth/index.js
@@ -0,0 +1,8 @@
+import * as actions from './authActions';
+import * as constants from './authConstants';
+import * as actionTypes from './authActionTypes';
+import reducer from './authReducer';
+
+import * as theme from '../../styles/theme';
+
+export { actions, constants, actionTypes, reducer, theme };
---
+++ modules/home/index.js
@@ -0,0 +1,8 @@
+import * as actions from './homeActions';
+import * as constants from './homeConstants';
+import * as actionTypes from './homeActionTypes';
+import reducer from './homeReducer';
+
+import * as theme from '../../styles/theme';
+
+export { actions, constants, actionTypes, reducer, theme };
