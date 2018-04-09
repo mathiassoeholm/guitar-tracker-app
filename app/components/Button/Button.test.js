ef7abfba243e04f510367da45a38c4df058672ef
Mathias <mathiassoeholm@gmail.com>
2018-04-09 21:29:21 +0200

    Added test that sees if button has text

--- app/components/Button/Button.test.js
+++ app/components/Button/Button.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import { Text } from 'react-native';
 
 import Button from './index';
 
 describe('List', () => {
   let button;
+  let clicked;
 
   beforeEach(() => {
-    button = TestRenderer.create(<Button text="My Button" />);
+    clicked = false;
+    button = TestRenderer.create(<Button text="My Button" onClick={() => { clicked = true; }} />);
   });
 
   it('renders', () => {
     expect(button.toJSON()).toBeTruthy();
+  });
+
+  it('displays the text', () => {
+    expect(button.root.findByType(Text).children[0].children[0]).toBe('My Button');
   });
 });
