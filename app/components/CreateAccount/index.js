ce37baac8bc202d2bdf162768cfaebe95d63343e
Mathias <mathiassoeholm@gmail.com>
2018-07-08 21:06:30 +0200

    Cleaned up CreateAccount

--- app/components/CreateAccount/index.js
+++ app/components/CreateAccount/index.js
@@ -1,46 +1,22 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import SvgUri from 'react-native-svg-uri';
+import { View } from 'react-native';
 
+import Header from '../Header';
+import Button from '../Button';
 import styles from './styles';
-import theme from '../../styles/theme';
 import svg from '../../assets/images/svg';
 
-const CreateAccount = () =>
-{
-  // Calculate size of image
-  const minButtonContainerHeight = 300;
-  const titleStyle = StyleSheet.flatten(styles.title);
-  const imageSize = Math.min(
-    theme.windowWidth,
-    theme.windowHeight -
-      (titleStyle.fontSize + titleStyle.paddingTop + titleStyle.paddingBottom) -
-      minButtonContainerHeight,
-  );
-
-    return (
-      <View style={styles.container}>
-		<Text style={styles.title}>
-		Create Account
-    </Text>
-        <SvgUri
-          width={(imageSize).toString()}
-          height={(imageSize).toString()}
-          svgXmlData={svg.electricGuitar}
-        />
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button}>
-            <Text style={theme.buttonTextStyle}>
-              CREATE ACCOUNTS
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.signInButton}>
-            <Text style={theme.buttonTextStyle}>
-              SIGN IN
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>);
-};
+const CreateAccount = () => (
+  <View style={styles.container}>
+    <Header svgData={svg.electricGuitar} minHeightForRest={400} title="Create Account" />
+    <View style={styles.buttonContainer}>
+      <Button
+        text="GO!"
+        style={styles.createButton}
+        onPress={() => console.log('Go!')}
+      />
+    </View>
+  </View>
+);
 
 export default CreateAccount;
